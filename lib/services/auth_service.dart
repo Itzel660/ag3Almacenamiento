01c6c094b62b85dc2f269d873fd32424f49347ff
VictorManuel211 <victormanuel160088@outlook.com>
2025-02-16 10:00:14 -0600

    Subiendo proyecto Flutter

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -5,23 +5,42 @@ class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
+  // Iniciar sesión con Google
   Future<User?> signInWithGoogle() async {
-    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-    if (googleUser == null) return null;
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) return null;
 
-    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-    final AuthCredential credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
 
-    UserCredential userCredential = await _auth.signInWithCredential(credential);
-    return userCredential.user;
+      UserCredential userCredential = await _auth.signInWithCredential(credential);
+      return userCredential.user;
+    } catch (e) {
+      print("Error en Google Sign-In: $e");
+      return null;
+    }
   }
 
+  // Iniciar sesión de manera anónima
+  Future<User?> signInAnonymously() async {
+    try {
+      UserCredential userCredential = await _auth.signInAnonymously();
+      return userCredential.user;
+    } catch (e) {
+      print("Error en inicio de sesión anónimo: $e");
+      return null;
+    }
+  }
+
+  // Cerrar sesión
   Future<void> signOut() async {
     await _auth.signOut();
     await _googleSignIn.signOut();
   }
 }
+
 
--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -1,56 +1,128 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../services/auth_service.dart';
 import 'home_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginScreen extends StatelessWidget {
   final AuthService _authService = AuthService();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  // Método para iniciar sesión con Google
-  Future<void> _signInWithGoogle(BuildContext context) async {
-    final user = await _authService.signInWithGoogle();
-    if (user != null) {
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeScreen()));
-    }
-  }
-
-  // Método para iniciar sesión de manera anónima (sin correo)
-  Future<void> _signInAnonymously(BuildContext context) async {
-    try {
-      await _auth.signInAnonymously();
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeScreen()));
-    } catch (e) {
-      print("Error en inicio de sesión anónimo: $e");
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text('Error al iniciar sesión'),
-        backgroundColor: Colors.red,
-      ));
-    }
-  }
+  LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Botón para iniciar sesión con Google
-            ElevatedButton(
-              onPressed: () => _signInWithGoogle(context),
-              child: Text('Iniciar sesión con Google'),
+      backgroundColor: const Color.fromARGB(255, 27, 27, 27),
+      body: Column(
+        children: [
+          const SizedBox(height: 150),
+          Center(
+            child: Image.asset(
+              'assets/logo.png',
+              width: 200,
+              height: 200,
             ),
-            SizedBox(height: 20),
-            // Botón para iniciar sesión como invitado (acceso anónimo)
-            ElevatedButton(
-              onPressed: () => _signInAnonymously(context),
-              child: Text('Entrar como invitado'),
+          ),
+          const SizedBox(height: 40),
+          const Center(
+            child: Text(
+              'App de libros',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
             ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 200), // Ajuste de espacio
+
+          // Botón de Google
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ElevatedButton(
+              onPressed: () async {
+                User? success = await _authService.signInWithGoogle();
+                if (success != null) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomeScreen()),
+                  );
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 40,
+                      height: 40,
+                      child: Image.asset(
+                        'assets/google.png',
+                        width: 30,
+                        height: 30,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    const Text(
+                      'Iniciar sesión con Google',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+          // Botón de acceso como invitado
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: ElevatedButton(
+              onPressed: () async {
+                User? success = await _authService.signInAnonymously();
+                if (success != null) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomeScreen()),
+                  );
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.grey[700],
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(Icons.person, color: Colors.white, size: 30),
+                    SizedBox(width: 10),
+                    Text(
+                      'Entrar como invitado',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
 
